f3758d1cd7f3a4e389e6e4360965c01c043749c6
www_vova <kotopes231@gmail.com>
2020-10-25 20:29:02 +0200

    fix default theme

--- resources/views/themes/default/site/pages/category.blade.php
+++ resources/views/themes/default/site/pages/category.blade.php
@@ -3,10 +3,10 @@
 @section('content')
     <section id="products"  class="flex flex-column items-center pt2 ph2 ph0-l">
         <div class="w-100 mw7 ">
-            @include('site.partials.sortForm')
+            @include('theme::site.partials.sortForm')
             <div class="flex flex-wrap">
                 @forelse($category->products as $product)
-                    @include('site.product.thumb')
+                    @include('theme::site.product.thumb')
                 @empty
                     <div class="ph2 ph3-l">Товары не найдены</div>
                 @endforelse
